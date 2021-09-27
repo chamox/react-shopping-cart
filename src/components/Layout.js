c3dfb6268bb1db097ee109907e948742d2b4f35c
samuel <sezuniga1@uc.cl>
2021-09-27 03:25:06 -0300

    Add: Navbar and chart

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 
 const styles = {
     layout:{
-        backgroundColor: 'fff',
+        backgroundColor: '#fff',
         color: '#0A283E',
         alignItems: 'center',
         display: 'flex',
